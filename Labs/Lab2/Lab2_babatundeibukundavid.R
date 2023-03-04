200ed8738f005aeb60ba54efe7e81e808906414c
[Ed Seabright] <ed.seabright@gmail.com>
2023-03-04 18:13:23 +0100

    Create Lab2_babatundeibukundavid.R

---
+++ Labs/Lab2/Lab2_babatundeibukundavid.R
@@ -0,0 +1,386 @@
+###################################
+###################################
+########                   ########
+########   Data Science 2  ########
+########       Lab 2       ######## 
+########  7th Feb. 2023    ########
+########                   ########
+###################################
+###################################
+
+
+# Welcome to lab number 2. Let's learn some new R concepts.
+
+########################
+####    Matrices    ####
+########################
+
+# In R, a MATRIX is a vector that has folded onto itself into a series of rows and columns. You can construct one from a normal vector:
+matrix(1:9, nrow = 3)
+
+# By default matrices get filled column by column, as you can see from the output. To fill them row by row instead, change the argument brow to TRUE:
+matrix(1:9, byrow = T, nrow = 3)
+
+# Now is a good time to point out that R will automatically read T as TRUE and F as FALSE, which can be a time-saver.
+
+# Let's define 3 vectors. Each one represents the box office numbers from the first three Star Wars movies. 
+# The first element of each vector indicates the US box office revenue,
+# the second element refers to the Non-US box office.
+
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Create box_office vector
+box_office <- c(new_hope, empire_strikes, return_jedi)
+
+# Construct star_wars_matrix
+star_wars_matrix <- matrix(box_office, byrow = T, nrow = 3)
+star_wars_matrix
+
+# we can give matrices rownames and column names
+
+rownames(star_wars_matrix) <- c("A new hope", "The empire strikes back", "Return of the Jedi")
+colnames(star_wars_matrix) <- c("US revenue", "International revenue")
+star_wars_matrix
+### 1.1
+# Some important base R functions to know: colSums, rowSums, colMeans, rowMeans
+# Use one of the above functions to calculate the total revenue for each movie (the sum of the US and international revenue)
+# and save it in an object called total_revenue
+total_revenue <- rowSums(star_wars_matrix)
+total_revenue
+
+# We can now add this vector as a new column using the function cbind (column bind)
+
+star_wars_matrix <- cbind(star_wars_matrix, total_revenue)
+star_wars_matrix
+### 1.2
+# Rename the 3rd element of the column names of star_wars_matrix to "Total revenue"
+colnames(star_wars_matrix)[3] <- "Total revenue"
+star_wars_matrix
+# Now lets create vectors for the box office returns of the prequel trilogy
+
+phantom_menace <- c(474.5,552.5)
+attack_of_clones <- c(310.7,338.7)
+revenge_of_sith <- c(380.3, 468.5)
+
+### 1.3
+# Turn these 3 vectors into a matrix, add a column for total revenue, 
+# and append them to star_wars_matrix using the function rbind (row bind)
+box_office <- c(phantom_menace, attack_of_clones, revenge_of_sith)
+movies_matrix <- matrix(box_office, byrow = T, nrow = 3)
+movies_matrix
+rownames(movies_matrix) <- c("phantom_menace", "attack_of_clones", "revenge_of_sith")
+movies_matrix
+total_revenue <- rowSums(movies_matrix)
+movies_matrix <- cbind(movies_matrix, total_revenue)
+movies_matrix
+
+#bind
+star_wars_matrix <- rbind(movies_matrix, star_wars_matrix)
+star_wars_matrix
+colnames(star_wars_matrix) <- c("US revenue", "International revenue", "Total_revenue")
+star_wars_matrix
+# Matrices are understood by R to be both one-dimensional, because they are vectors folded onto themselves
+# into columns, but also 2 dimensional, because they have rows and columns. 
+# So you can index them like this:
+star_wars_matrix[4]
+
+# but also like this
+star_wars_matrix[3,1]
+
+# When you put a comma in between square brackets, you are indexing both the rows (to the left of the comma),
+# and the columns (to the right of the comma).
+
+### 1.4 
+# Write a line of code to extract the international revenue of the Phantom Menace (the 4th movie) using numbers to index the matrix
+star_wars_matrix[1,2]
+# Compare this to the following:
+star_wars_matrix["phantom_menace", "Total_revenue"]
+
+# This works because our matrix has row and column names.
+
+# If you try to include a comma in the index of a one-dimensional object (like a vector),
+# you get an error message:
+new_hope[1,2]
+
+# If you leave a blank space either to the left or the right of the comma, you select for ALL the rows
+# or ALL the columns, respectively
+star_wars_matrix[,2]
+star_wars_matrix[4,]
+
+# Just like multiplying 2 vectors of the same length together will multiply each of the corresponding elements
+# the same will work with 2 matrices of the same dimensions
+
+star_wars_matrix[1:3,] * star_wars_matrix[4:6,] 
+
+# But for matrix multiplication (the kind used in linear algebra), you must use %*%
+
+#####################
+####    Lists    ####
+#####################
+
+# Lists are extremely useful objects in R. 
+# With a list, you can save lots of different objects into one object.
+
+my_list <- list(c("red,green"), 1:5, FALSE, matrix(1:9,3,3))
+my_list
+
+my_list[3]
+
+# You can even have lists inside of lists, recursively
+my_list_2 <- list(my_list,c("Hello","World"))
+my_list_2
+
+# You cane name the elements of a list with names()
+names(my_list) <- c("colours","digits","boolean","matrix")
+my_list
+
+# and you can access each element of a list with the $ operator
+my_list$boolean
+
+### 2.1
+# Using the $ operator, replace the "matrix" element of my_list with the star_wars_matrix
+my_list$matrix <- star_wars_matrix
+my_list
+# Finally, you can turn any list into a vector with unlist().
+unlist(my_list)
+
+# If the classes of each of the elements are different, it will automatically convert them all 
+# to the 'character' class so they are all compatible
+
+###########################
+####    Data frames    ####
+###########################
+
+# Data frames are also 2-dimensional objects, with rows and columns.
+# However, data frames are not, like matrices, single vectors folded onto themselves.
+# Instead, they are special kinds of list, where each element of the list is a vector 
+# of the same length, but possibly of different classes, each of which is a column of the
+# data frame.
+
+# Most data you will work with in R will be in a data frame. 
+# Lets load one that comes automatically with your R installation:
+
+data("iris")
+class(iris)
+
+# just like with functions, we can learn about these in-built data frames with the ? sign
+?iris
+
+# but even when this information isn't at hand, we can run:
+str(iris)
+head(iris)
+summary(iris)
+
+# which each give you different information about the data frame.
+
+# Just like lists, you can call each column using the $ operator
+iris$Species
+
+# Let's do some basic data visualization in R:
+# First, a histogram
+hist(iris$Sepal.Length)
+
+# Next, a scatterplot
+plot(iris$Petal.Length,iris$Petal.Width)
+
+
+###############################
+####    More Randomness    ####
+###############################
+
+## SETTING THE SEED
+# Remember this?
+sample(1:100,1)
+sample(1:100,1)
+sample(1:100,1)
+
+# Those of you with background in computer science or data science probably know
+# that true randomness doesn't really exist in computers. Instead, the appearance of randomness
+# is created by generating a different 'seed' number at every different point in time
+# often generated by the actual internal clock of the computer
+# and basing all 'random' sampling from this seed.
+# As a result, we can set our own seed and generate our own randomness:
+
+set.seed(123)
+sample(1:100,1)
+
+# You got 31, right? Run it again and see:
+
+set.seed(123)
+sample(1:100,1)
+
+# Any time we want to make sure we get the same results as one another despite having some
+# element of random sampling in out code, we will set the seed to be the same.
+
+## RANDOM DISTRIBUTIONS 
+
+# Remember the formula for the Binomial distribution:
+# 𝑷(W=k) =  𝒑^𝒌∗(𝟏−𝒑)^((𝑵−𝒌) )∗(𝒏¦𝒌)
+# Where (𝒏¦𝒌)=  𝒏!/𝒌!(𝒏−𝒌)!
+
+### 4.1
+# Write a function to calculate the probability of exactly k successes
+# out of n attempts each with probability p of success,
+# where n, k and p are arguments of the function
+# To do this you will need to use either the factorial() function
+# or the choose() function
+binomial_prob <- function(n, k, p) {
+  # calculate the binomial coefficient using the factorial function
+  binom_coef <- factorial(n) / (factorial(k) * factorial(n - k))
+  # calculate the probability using the Binomial distribution formula
+  prob <- binom_coef * p^k * (1 - p)^(n - k)
+  return(prob)
+}
+
+# use your function to calculate the probability that when the aliens send 10 probes to Earth (probability of water = 0.7),
+# exactly 8 of those probes will send a signal of water
+# define the parameters
+n <- 10   # number of probes
+k <- 8    # number of probes that send a signal of water
+p <- 0.7  # probability of water for each probe
+
+# calculate the probability using the binomial_prob() function
+prob <- binomial_prob(n, k, p)
+
+# print the result
+prob
+
+
+# compare this to dbinom(8,10,0.7)
+dbinom(8,10,0.7)
+### PROBABILITY FUNCTIONS IN R 
+# dbinom, dnorm, dunif, dbeta, .... all of these functions calculate f(x) for any given x
+# for each of their relative distributions
+# in other words, they calculate probability MASS or probability DENSITY for each of
+# these distributions
+
+# rbinom, rnorm, runif, rbeta, .... all SAMPLE from these respective distributions,
+# i.e. they generate random draws of x in proportion to their respective f(x) in these distributions.
+
+### 4.2
+# The aliens send 20 probes to an unknown planet.
+# 11 of them signal Water, the rest signal Land
+# They want to know if this planet could possibly be Earth
+# Their main astronomer thinks it is unlikely, as Earth has 70% water
+# and less than 60% of the probes signalled water
+# She asks the statistician: if this planet IS Earth,
+# what is the probability that the probes would have returned
+# this few Water signals (or fewer).
+# Using rbinom(), simulate 100,000 universes where the aliens sent out 20 probes to Earth
+# and calculate in what percentage of these universes the number of probes signalling Water is 11 or fewer
+
+# Define the parameters
+n <- 20   # number of probes sent out
+k <- 11   # number of probes signalling water
+p <- 0.7  # probability of water on Earth
+
+# Simulate 100,000 universes
+set.seed(123)  # set the seed for reproducibility
+num_sims <- 100000
+num_water <- rbinom(num_sims, n, p)
+
+# Calculate the proportion of universes where we get 11 or fewer water signals
+prop_water_11_or_fewer <- sum(num_water <= 11) / num_sims
+
+# Print the result
+prop_water_11_or_fewer
+# What do you conclude to the astronomer?
+#This suggests that if the planet being observed is Earth, the probability of 
+#getting 11 or fewer water signals out of 20 probes is quite low (less than 0.05%).
+#Therefore, the astronomer may conclude that it is unlikely that the planet being
+#observed is Earth, given the observed data.
+
+# pbinom, pnorm, punif, pbeta, .... all calculate the area under the curve of a given distribution,
+# in the LOWER tail (if lower.tail=TRUE, by default), or the UPPER tail (if you set it to false)
+
+# So for example, if a person of 195 cm was drawn from a gaussian distribution of heights,
+# with mean 175cm and standard deviation 10cm, What percentile would they be in?
+pnorm(195, 175, 10, lower.tail=FALSE)
+# They would be in the upper 2.3 percentile
+
+# qbinom, qnorm, qunif, qbeta, .... all calculate value of x for which
+# the area under the curve of a given distribution in the lower tail 
+# is equal to the given number
+
+# For example, in a Gaussian distribution of people's heights with mean 175cm and standard deviation 10cm,
+# How tall is someone in the bottom 10th percentile? 
+qnorm(0.1, 175, 10)
+# In the upper 10th percentile?
+qnorm(0.9, 175, 10)
+# Compare this to:
+qnorm(0.1,175, 10, lower.tail=FALSE)
+
+### 4.3
+# Let's compare the box office returns of the of and prequel trilogies
+og_trilogy <- star_wars_matrix[1:3,3]
+preq_trilogy <- star_wars_matrix[4:6,3]
+
+# Write a Welch's t-test function for any two samples x1 and x2
+my_t <- function(x1,x2){
+  # first, extract the means, variances and Ns of the two samples and save them to
+  n1 <- length(x1)
+  m1 <- mean(x1)
+  s1 <-  var(x1)
+  n2 <- length(x2)
+  m2 <- mean(x2)
+  s2 <- var(x2)
+ 
+  # next, calculate the average standard deviation using the formula shown in the class on slide 44:
+ 
+  s <- 
+
+  # next, calculate the t-statistic, again as shown on slide 44
+ 
+  t <- 
+ 
+ 
+  # next, calculate the degrees of freedom (again see slide 44)
+  # make sure you use parentheses correctly here
+ 
+  df <- 
+ 
+  # next, calculate the probability that the t-statistic would be greater than the absolute value of the t-statistic that you calculated if the TRUE difference between the groups was 0
+  # to do this, you can use function pt
+  p_value <- pt(abs(t), df=df, lower.tail = F)*2
+
+  return(list(t = t, df = df, p_value=p_value))
+  }
+
+
+
+#the codes
+my_t <- function(x1, x2) {
+  # Calculate sample sizes
+  n1 <- length(x1)
+  n2 <- length(x2)
+  
+  # Calculate sample means
+  m1 <- mean(x1)
+  m2 <- mean(x2)
+  
+  # Calculate sample variances
+  s1 <- var(x1)
+  s2 <- var(x2)
+  
+  # Calculate Welch's t-test statistic
+  t_stat <- (m1 - m2) / sqrt(s1/n1 + s2/n2)
+  
+  # Calculate degrees of freedom using Welch-Satterthwaite formula
+  df <- (s1/n1 + s2/n2)^2 / ((s1/n1)^2/(n1-1) + (s2/n2)^2/(n2-1))
+  
+  # Calculate two-tailed p-value using cumulative density function
+  p_value <- 2 * pt(abs(t_stat), df)
+  
+  # Return a list with the t-statistic, degrees of freedom, and p-value
+  return(list(t_stat = t_stat, df = df, p_value = p_value))
+}
+
+
+# compare this function to the in-built t-test
+t.test(og_trilogy,preq_trilogy)
+my_t(og_trilogy,preq_trilogy)
+
+# One last question to ponder before next class: Why did we multiply the p-value by 2?
